59af92377ad77be58b4f392f5e0b5dbac35e807f
MihaelGolob <golob.mihael@gmail.com>
2022-09-14 09:56:52 +0200

    added conditionals to the log methods so you can strip them out on release and also added a callback method that logs all exception messages too

--- Runtime/Log.cs
+++ Runtime/Log.cs
@@ -2,14 +2,31 @@ using System;
 using Serilog;
 using Serilog.Sinks.Unity3D;
 
-public class Log {
+#if UNITY_EDITOR 
+using UnityEngine;
+#endif
+
+public static class Log {
     // internal variables
-    private static ILogger _logger;
+    private static Serilog.ILogger _logger;
     
     // public properties
     
+    // static constructor
+    static Log() {
+        // subscribe to events
+        Application.logMessageReceivedThreaded += LogException;
+    }
+    
+    // ------------------------------ P R I V A T E -------------------------------------
+    private static void LogException(string log, string stackTrace, LogType type) {
+        if (type != LogType.Exception) return;
+        _logger.Error(log);
+        _logger.Error(stackTrace);
+    }
+    
     // ------------------------------ P U B L I C    A P I ------------------------------
-    public static void Init(bool logToUnityConsole = true, bool logToFile = true) {
+    public static void Init(bool logToUnityConsole = true, bool logToFile = true, string fileName = null) {
         // create logger configuration
         // enable debug messages with MinimumLevel
         var loggerConfiguration = new LoggerConfiguration().MinimumLevel.Verbose();
@@ -19,7 +36,10 @@ public class Log {
             loggerConfiguration.WriteTo.Unity3D();
         }
         if (logToFile) {
-            loggerConfiguration.WriteTo.File($"Log_{DateTime.Now.ToString("dd.MM.yyyy_HH-mm-ss")}.txt");
+            if (fileName == null)
+                loggerConfiguration.WriteTo.File($"Log_{DateTime.Now.ToString("dd.MM.yyyy_HH-mm-ss")}.txt");
+            else
+                loggerConfiguration.WriteTo.File(fileName);
         }
         
         // create logger
@@ -27,133 +47,174 @@ public class Log {
     }
     
     // Debug methods
-    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug (string message) 
         => _logger.Debug(message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T> (string message, T value) 
         => _logger.Debug(message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1> (string message, T0 value0, T1 value1) 
         => _logger.Debug(message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2> (string message, T0 value0, T1 value1, T2 value2) 
         => _logger.Debug(message, value0, value1, value2);
 
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3> (string message, T0 value0, T1 value1, T2 value2, T3 value3) 
         => _logger.Debug(message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3, T4> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4) 
         => _logger.Debug(message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3, T4, T5> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Debug(message, value0, value1, value2, value3, value4, value5);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug (Exception exception, string message) 
         => _logger.Debug(exception, message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T> (Exception exception, string message, T value)
         => _logger.Debug(exception, message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1> (Exception exception, string message, T0 value0, T1 value1)
         => _logger.Debug(exception, message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2> (Exception exception, string message, T0 value0, T1 value1, T2 value2)
         => _logger.Debug(exception, message, value0, value1, value2);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3)
         => _logger.Debug(exception, message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3, T4> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4)
         => _logger.Debug(exception, message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Debug<T0, T1, T2, T3, T4, T5> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Debug(exception, message, value0, value1, value2, value3, value4, value5);
     
     
     // Warning methods
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning (string message) 
         => _logger.Warning(message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T> (string message, T value) 
         => _logger.Warning(message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1> (string message, T0 value0, T1 value1) 
         => _logger.Warning(message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2> (string message, T0 value0, T1 value1, T2 value2) 
         => _logger.Warning(message, value0, value1, value2);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3> (string message, T0 value0, T1 value1, T2 value2, T3 value3) 
         => _logger.Warning(message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3, T4> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4) 
         => _logger.Warning(message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3, T4, T5> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Warning(message, value0, value1, value2, value3, value4, value5);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning (Exception exception, string message) 
         => _logger.Warning(exception, message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T> (Exception exception, string message, T value)
         => _logger.Warning(exception, message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1> (Exception exception, string message, T0 value0, T1 value1)
         => _logger.Warning(exception, message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2> (Exception exception, string message, T0 value0, T1 value1, T2 value2)
         => _logger.Warning(exception, message, value0, value1, value2);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3)
         => _logger.Warning(exception, message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3, T4> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4)
         => _logger.Warning(exception, message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Warning<T0, T1, T2, T3, T4, T5> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Warning(exception, message, value0, value1, value2, value3, value4, value5);
     
     // Error methods 
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error (string message) 
         => _logger.Error(message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T> (string message, T value) 
         => _logger.Error(message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1> (string message, T0 value0, T1 value1) 
         => _logger.Error(message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2> (string message, T0 value0, T1 value1, T2 value2) 
         => _logger.Error(message, value0, value1, value2);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3> (string message, T0 value0, T1 value1, T2 value2, T3 value3) 
         => _logger.Error(message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3, T4> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4) 
         => _logger.Error(message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3, T4, T5> (string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Error(message, value0, value1, value2, value3, value4, value5);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error (Exception exception, string message) 
         => _logger.Error(exception, message);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T> (Exception exception, string message, T value)
         => _logger.Error(exception, message, value);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1> (Exception exception, string message, T0 value0, T1 value1)
         => _logger.Error(exception, message, value0, value1);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2> (Exception exception, string message, T0 value0, T1 value1, T2 value2)
         => _logger.Error(exception, message, value0, value1, value2);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3)
         => _logger.Error(exception, message, value0, value1, value2, value3);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3, T4> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4)
         => _logger.Error(exception, message, value0, value1, value2, value3, value4);
     
+    [System.Diagnostics.Conditional("ENABLE_LOGS")]
     public static void Error<T0, T1, T2, T3, T4, T5> (Exception exception, string message, T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         => _logger.Error(exception, message, value0, value1, value2, value3, value4, value5);
     
